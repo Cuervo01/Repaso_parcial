0e7e2ee9f3b14c86befeff74ed9a4e4fd121ef07
simon <juansimonhv0@gmail.com>
2022-08-16 09:14:02 -0500

    Corrección código para Arduino

--- task1.cpp
+++ task1.cpp
@@ -1,40 +1,58 @@
 #include <Arduino.h>
 #include "task1.h"
 
+static void changeVar(uint32_t *pdata)
+{
+*pdata = 10;
+}
 
-void task1(){
-    enum class Task1States{
-        INIT,
-        WAIT_TO_TOGGLE_LED
-    };
-    static Task1States task1State = Task1States::INIT;
-    static uint32_t lasTime;
-    static constexpr uint32_t INTERVAL = 100;
-    static constexpr uint8_t ledRed = 3;
-    static bool ledStatus = false;
+static void printVar(uint32_t value)
+{
 
-    switch(task1State){
-        case Task1States::INIT:{
-            pinMode(ledRed,OUTPUT);
-            lasTime = millis();
-            task1State = Task1States::WAIT_TO_TOGGLE_LED;
-            break;
-        }
+Serial.print("var content: ");
+Serial.print(value);
+Serial.print("\n");
 
-        case Task1States::WAIT_TO_TOGGLE_LED:{
-            // evento 1:
-            uint32_t currentTime = millis();
-            if( (currentTime - lasTime) >= INTERVAL ){
-                lasTime = currentTime;
-                digitalWrite(ledRed,ledStatus);
-                ledStatus = !ledStatus;
-            }
-            break;
-        }
+//printf("var content: %d\n", (int)value);
+}
 
-        default:{
-            break;
-        }
-    }
+void task1()
+{
+enum class Task1States
+{
+INIT,
+WAIT_DATA
+};
+static Task1States task1State = Task1States::INIT;
 
+switch (task1State)
+{
+case Task1States::INIT:
+{
+Serial.begin(115200);
+task1State = Task1States::WAIT_DATA;
+break;
+}
+
+case Task1States::WAIT_DATA:
+{
+// evento 1:
+// Ha llegado al menos un dato por el puerto serial?
+if (Serial.available() > 0)
+{
+Serial.read();
+uint32_t var = 0;
+uint32_t *pvar = &var;
+printVar(*pvar);
+changeVar(pvar);
+printVar(var);
+}
+break;
+}
+
+default:
+{
+break;
+}
+}
 }
